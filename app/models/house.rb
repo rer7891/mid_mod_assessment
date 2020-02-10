a433ab683d3f99c45f45139da1163b47f3f2b8ed
Becky Robran <rerobran@gmail.com>
2020-02-10 10:29:58 -0700

    fixes house vs character endpoint

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,9 @@
 class House
+  attr_reader :name, :members, :role, :patronus
   def initialize(data)
+    @house = data["house"]
+    @name = data["name"]
+    @role = data["role"]
+    @patronus = data["patronus"]
   end
 end
