7e8d2c1615a219625bf8796f5aa7aa6ec3edcb50
Becky Robran <rerobran@gmail.com>
2020-02-10 09:27:58 -0700

    adds test for house search

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+
+feature "user can search for the Gryffindor house" do
+
+  scenario "user should see list of members for house and OP" do
+
+    visit '/'
+
+    select "Gryffindor", from: :house
+
+    click_on "Search For Members"
+
+    expect(current_path).to eq(search_path)
+
+    expect(page).to have_content("21 Gryffindor Members")
+
+    expect(page).to have_css(".member", count: 21)
+
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role")
+      expect(page).to have_css(".house")
+      expect(page).to have_css(".patronus")
+    end
+
+    expect(page).to have_css(".order", count: 21)
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role")
+      expect(page).to have_css(".house")
+      expect(page).to have_css(".patronus")
+    end
+
+  end
+end
