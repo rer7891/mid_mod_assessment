9b7def51d1730a7bdbaab6b9d4564d39885a5980
Becky Robran <rerobran@gmail.com>
2020-02-10 10:30:09 -0700

    fixes endpoint confusion

--- app/services/house_service.rb
+++ app/services/house_service.rb
@@ -1,13 +1,13 @@
 class HouseService
-  def members_by_house
-    response = conn.get("/v1/houses/5a05e2b252f721a3cf2ea33f?key=$2a$10$2qWJ47twXwT///yIYlqyROQic3oAp7pyzWm9QBsCCHEGebSkjNyGS")
+  def members_by_house(house)
+    response = conn.get("/v1/characters/?key=$2a$10$2qWJ47twXwT///yIYlqyROQic3oAp7pyzWm9QBsCCHEGebSkjNyGS&house=#{house}&orderOfThePhoenix=true")
     body = JSON.parse(response.body)
   end
 
 private
 
-  def connenction
-      conn = Faraday.new(url: 'https://www.potterapi.com') do |faraday|
+  def conn
+      Faraday.new(url: 'https://www.potterapi.com') do |faraday|
       faraday.adapter Faraday.default_adapter
     end
   end
